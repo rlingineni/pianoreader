3ead94eaacb87ea2d1cdeeaa23855fe4c27cda0f
Ravi Lingineni <rlingineni@live.com>
2024-11-09 17:06:24 -0800

    wip

--- react-src/src/components/KeyFrameEditor.js
+++ react-src/src/components/KeyFrameEditor.js
@@ -1,12 +1,15 @@
 import { useCallback, useEffect, useState } from "react";
 
-// const cx = (val) => {
-//     className={cx("px-2 py-1 text-sm", {
-//         "bg-gray-100": idx % 2,
-//       })}
-
-//
-// };
+/**
+ * Main TO:DO
+ * - Have toggle-able HTML/CSS Editor
+ * - HTML, every element id or class prefixed with a 'ani-', will show up in Unique items
+ * - CSS is read and items KeyFrameData const is populated with initial CSS Data
+ * - Future: CSS is generated with keyframe at point X on Double Click
+ * -
+ *
+ *
+ */
 
 const KeyFrameData = {
   0: {
@@ -21,7 +24,31 @@ const KeyFrameData = {
       translateX: 0,
     },
   },
-  1: {
+  15: {
+    "Button Group 1": {
+      opacity: 3,
+    },
+    "Button Group 3": {
+      translateX: 1,
+    },
+  },
+  25: {
+    "Button Group": {
+      opacity: 5,
+    },
+    "Button Group 2": {
+      opacity: 1,
+    },
+    "Button Group 3": {
+      opacity: 3,
+    },
+  },
+  45: {
+    "Button Group 1": {
+      opacity: 5,
+    },
+  },
+  100: {
     "Button Group": {
       opacity: 1,
     },
@@ -39,6 +66,18 @@ const UniqueElements = [
   "Button Group 3",
 ];
 
+function cx(str, obj) {
+  let className = str;
+
+  for (const key in obj) {
+    if (!!obj[key]) {
+      className = className + " " + key;
+    }
+  }
+
+  return className;
+}
+
 function KeyFrameEditor() {
   const [selectedElements, setSelectedElements] = useState([]);
 
@@ -49,9 +88,9 @@ function KeyFrameEditor() {
     }
 
     if (event.ctrlKey) {
-      setSelectedElements((els) => els.concat([el]));
-    } else {
       setSelectedElements([el]);
+    } else {
+      setSelectedElements((els) => els.concat([el]));
     }
 
     // if the ctrl key is held down, add to array
@@ -63,7 +102,7 @@ function KeyFrameEditor() {
     const properties = {};
 
     const times = Object.keys(KeyFrameData);
-    for (const time in times) {
+    for (const time of times) {
       if (KeyFrameData[time][el]) {
         for (const key of Object.keys(KeyFrameData[time][el])) {
           if (!properties[key]) {
@@ -80,15 +119,76 @@ function KeyFrameEditor() {
     return properties;
   };
 
-  const Timeline = ({ events }) => {
+  const filterKeyFrameData = (el, propertiesToFilter) => {
+    const filteredSet = {};
+    const times = Object.keys(KeyFrameData);
+    for (const time of times) {
+      if (KeyFrameData[time][el]) {
+        if (!filteredSet[time]) {
+          filteredSet[time] = { [el]: {} };
+        }
+        const animatedProperties = Object.keys(KeyFrameData[time][el]);
+        if (propertiesToFilter && propertiesToFilter.length > 0) {
+          let exists = false;
+          for (const property of propertiesToFilter) {
+            if (animatedProperties.includes(property)) {
+              filteredSet[time][el][property] =
+                KeyFrameData[time][el][property];
+              exists = true;
+            }
+          }
+
+          if (!exists) {
+            delete filteredSet[time][el];
+            delete filteredSet[time];
+          }
+        } else {
+          filteredSet[time][el] = KeyFrameData[time][el];
+        }
+      }
+    }
+
+    return filteredSet;
+  };
+
+  /**
+   * Renders a timeline
+   * To-DO: Add Event Click Handlers for each keyframe
+   * @returns
+   */
+  const Timeline = ({ el, properties, selected, subTimeline, dim }) => {
+    const timeStamps = Object.keys(filterKeyFrameData(el, properties));
     return (
-      <div className="relative h-1 bg-red-200 rounded-md w-full">
-        {events.map((e) => (
+      <div
+        className={cx("relative h-1 rounded-md w-full", {
+          "bg-red-200": !subTimeline,
+          "bg-blue-200": subTimeline,
+          "opacity-40": dim,
+        })}
+      >
+        {timeStamps.map((e) => (
           <div
             className="absolute w-1 h-1 bg-red-500"
-            style={{ left: e.time / 100 }}
+            style={{ left: `${e}%` }}
           />
         ))}
+      </div>
+    );
+  };
+
+  /**
+   * Draws a playhead
+   * To-Do: Pass Percent Moved on OnMove funciton, and then pass it in the state again to abs. position
+   * @param {Draws} percent
+   * @returns
+   */
+  const PlayHead = (percent) => {
+    return (
+      <div
+        className="absolute w-1 h-72 bg-green-500 relative"
+        style={{ left: `${10}%` }}
+      >
+        <div className="absolute bg-green-500 w-3 h-2 top-0 -left-1 rounded-full" />
       </div>
     );
   };
@@ -105,28 +205,34 @@ function KeyFrameEditor() {
 
   return (
     <div className="w-full h-full">
-      <div className="flex border-bottom border-gray-200 mb-1 gap-2">
-        <div className="bg-gray-200 px-2 py-.5 text-sm border-b-2 border-gray-500 w-32  flex items-center">
-          Entry Scene
+      <div className="h-72 w-full bg-gray-200 mb-4">CANVAS</div>
+
+      <div className="w-full flex justify-between mb-1">
+        <div className="flex border-bottom border-gray-200 gap-2">
+          <div className="bg-gray-200 px-2 py-.5 text-sm border-b-2 border-gray-500 w-32  flex items-center">
+            Entry Scene
+          </div>
+          <div className="bg-gray-100 px-2 py-.5 text-md flex justify-center items-center rounded-md">
+            +
+          </div>
         </div>
-        <div className="bg-gray-100 px-2 py-.5 text-md flex justify-center items-center rounded-md">
-          +
-        </div>
+        <div className="bg-blue-200 px-2 py-.5">Play</div>
       </div>
 
       <div className="flex w-full h-full">
-        <div className="pr-6 bg-gray-100">
+        <div className="w-48 bg-gray-100">
           <div className="h-6 bg-gray-200 w-full" />
           {UniqueElements.map((el, idx) => (
             <div>
-              <div
-                className="px-2 py-1.5 text-sm"
-                key={el}
-                onClick={(e) => {
-                  toggleSelectedElements(el, e);
-                }}
-              >
-                {el}
+              <div className="px-1 py-1.5 text-xs" key={el}>
+                <div
+                  className="font-bold"
+                  onClick={(e) => {
+                    toggleSelectedElements(el, e);
+                  }}
+                >
+                  {el}
+                </div>
                 {selectedElements.includes(el) &&
                   Object.keys(getSubProperties(el)).map((key) => (
                     <div className="mt-3 pl-2 text-xs">{key}</div>
@@ -135,22 +241,23 @@ function KeyFrameEditor() {
             </div>
           ))}
         </div>
-        <div className="w-3/4 bg-gray-400">
-          <div className="h-6 bg-gray-200"> </div>
+        <div className="w-full bg-gray-400">
+          <div className="h-2 bg-gray-200">
+            <div className="mx-2 bg-green-200 h-2 relative">
+              <PlayHead />
+            </div>
+          </div>
           {UniqueElements.map((el, idx) => (
-            <div className="w-full">
-              <div className="px-2 py-1.5">
-                <span class="flex items-center w-full text-sm">
-                  &nbsp;
-                  <div className="h-2 bg-red-200 rounded-md w-full" />
-                </span>
-                {/* {selectedElements.includes(el) && {}
-                 Object.keys(getSubProperties(el)).map((key) => (
-                    <div className="mt-3 text-sm">
-                      <Timeline events={getSubProperties(el)} />
-                    </div>
-                  ))} */}
-              </div>
+            <div className="px-2 w-full" style={{ marginTop: "2px" }}>
+             <span className="pl-2">
+             <Timeline el={el} dim={selectedElements.includes(el)}/> 
+              </span>
+              {selectedElements.includes(el) &&
+                Object.keys(getSubProperties(el)).map((key) => (
+                  <span className="mt-3 pl-2">
+                    <Timeline el={el} properties={[key]} subTimeline={true} />
+                  </span>
+                ))}
             </div>
           ))}
         </div>
